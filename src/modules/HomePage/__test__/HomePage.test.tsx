933a65f3535a08709211563dafbba8f41221da0f
cthacker-udel <cthacker@udel.edu>
2023-03-02 04:56:55 -0500

    [main] Added first unit test

--- src/modules/HomePage/__test__/HomePage.test.tsx
+++ src/modules/HomePage/__test__/HomePage.test.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import { HomePage } from "../HomePage";
+import { TextConstants } from "@/assets/str/TextConstants";
 
 jest.mock("next/router", () => require("next-router-mock"));
 
 describe("test1", () => {
   it("render", () => {
     render(<HomePage />);
+    const homePageDescription = document.getElementById(
+      "homepage_header_description"
+    );
+    expect(homePageDescription).not.toBeNull();
+    expect(homePageDescription?.innerHTML).toEqual(
+      TextConstants.HOME_PAGE.DESCRIPTION
+    );
   });
 });
